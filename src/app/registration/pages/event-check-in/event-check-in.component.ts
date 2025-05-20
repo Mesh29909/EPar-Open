d1607187f6ea5d0ac926f878c9b0717cf0ced227
Jean Pierr <u202120005@upc.edu.pe>
2025-05-20 06:47:37 -0500

    z

---
+++ src/app/registration/pages/event-check-in/event-check-in.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-event-check-in',
+  imports: [],
+  templateUrl: './event-check-in.component.html',
+  styleUrl: './event-check-in.component.css'
+})
+export class EventCheckInComponent {
+
+}

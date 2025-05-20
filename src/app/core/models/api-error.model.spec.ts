769ceaadcbb809191e2cc6ca208d6ea0483a700c
Jean Pierr <u202120005@upc.edu.pe>
2025-05-20 06:45:05 -0500

    z

---
+++ src/app/core/models/api-error.model.spec.ts
@@ -0,0 +1,7 @@
+import { ApiErrorModel } from './api-error.model';
+
+describe('ApiErrorModel', () => {
+  it('should create an instance', () => {
+    expect(new ApiErrorModel()).toBeTruthy();
+  });
+});

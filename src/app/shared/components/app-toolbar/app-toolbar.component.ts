eef7efb617a04202e193d1b988c12928970d9e80
Jean Pierr <u202120005@upc.edu.pe>
2025-05-20 06:50:44 -0500

    z

---
+++ src/app/shared/components/app-toolbar/app-toolbar.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-app-toolbar',
+  imports: [],
+  templateUrl: './app-toolbar.component.html',
+  styleUrl: './app-toolbar.component.css'
+})
+export class AppToolbarComponent {
+
+}

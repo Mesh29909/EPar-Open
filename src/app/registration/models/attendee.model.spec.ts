c39b388dd87a3466ecd6a6c34b79171058b9566d
Jean Pierr <u202120005@upc.edu.pe>
2025-05-20 06:46:21 -0500

    z

---
+++ src/app/registration/models/attendee.model.spec.ts
@@ -0,0 +1,7 @@
+import { AttendeeModel } from './attendee.model';
+
+describe('AttendeeModel', () => {
+  it('should create an instance', () => {
+    expect(new AttendeeModel()).toBeTruthy();
+  });
+});
